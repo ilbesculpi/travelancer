855c4a792750ecb036b272e631b7173ca7c896f5
Ilbert Esculpi <ilbert.esculpi@gmail.com>
2017-09-11 23:22:37 -0400

    home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title><?= env('APP_NAME', 'Laravel') ?></title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -66,30 +66,21 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
+			
             <div class="content">
+				
                 <div class="title m-b-md">
-                    Laravel
+                    <?= env('APP_NAME', 'Laravel') ?>
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="<?= url('/admin') ?>">Dashboard</a>
+                    <a href="<?= url('/docs') ?>">Documentation</a>
+                    <a href="<?= url('/api/docs') ?>">API</a>
                 </div>
+				
             </div>
+			
         </div>
     </body>
 </html>
